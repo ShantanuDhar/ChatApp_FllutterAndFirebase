9f2de86782f5e6adcbe9593ad442b1a8d514e111
ShantanuDhar <shantanu170604@gmail.com>
2024-03-30 14:57:59 +0530

    Adding drawer to homepage and getting data from shared preference

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,37 +1,84 @@
-import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:chatapp_firebase/helper/helper_function.dart';
 import 'package:chatapp_firebase/pages/search_page.dart';
 import 'package:chatapp_firebase/service/auth_service.dart';
 import 'package:chatapp_firebase/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  String email = "";
+  String name = "";
+  @override
+  void initState(){
+    super.initState();
+    getUserDetails();
+  }
+  void getUserDetails() async{
+    await HelperFunction.getUserEmail().then((value){
+      setState(() {
+        email = value!;
+      });
+    });
+    await HelperFunction.getUserName().then((value){
+      setState(() {
+        name = value!;
+      });
+    });
+  }
+  
   AuthService auth = AuthService();
+  
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-              onPressed: () {
-                nextScreen(context, const SearchPage());
-              },
-              icon: const Icon(
-                Icons.search,
-              ))
+            onPressed: () {
+              nextScreen(context, const SearchPage());
+            },
+            icon: const Icon(
+              Icons.search,
+            ),
+          )
         ],
         elevation: 0,
         centerTitle: true,
         backgroundColor: Theme.of(context).primaryColor,
-        title: const Text("Groups",style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),),
-        
+        title: const Text(
+          "Groups",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 27,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 150,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 15),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+  
