c8128778944de97b5b8eeab7f2d7060083ce71fd
ShantanuDhar <shantanu170604@gmail.com>
2024-04-03 00:52:09 +0530

    Adding user groups

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -187,9 +187,61 @@ class _HomePageState extends State<HomePage> {
   }
    popUpDialog(BuildContext context) {
    }
-  groupList(){
-
+    groupList() {
+    return StreamBuilder(
+      stream: groups,
+      builder: (context, AsyncSnapshot snapshot) {
+        // make some checks
+        if (snapshot.hasData) {
+          if (snapshot.data['groups'] != null) {
+            if (snapshot.data['groups'].length != 0) {
+             return Text("hello");
+                
+            } else {
+              return noGroupwidget();
+            }
+          } else {
+            return noGroupwidget();
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor),
+          );
+        }
+      },
+    );
   }
-}
 
-  
+
+    
+noGroupwidget(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 25),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+              popUpDialog(context);
+            },
+            child: Icon(
+              Icons.add_circle,
+              color: Colors.grey[700],
+              size: 75,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "You've not joined any groups, tap on the add icon to create a group or also search from top search button.",
+            textAlign: TextAlign.center,
+          )
+        
+      ],
+    )
+  );
+}
+}
