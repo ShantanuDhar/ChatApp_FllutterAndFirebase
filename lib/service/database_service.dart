582eb071af4bf97b7be0dccfa14fb0d98669d990
ShantanuDhar <shantanu170604@gmail.com>
2024-04-08 21:29:19 +0530

    Changing DB2

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -28,19 +28,16 @@ class DatabaseService {
    return userCollection.doc(uid).snapshots();
   }
   Future createGroup(String userName,String id,String groupName) async{
-     DocumentReference groupDocumentReference = await groupCollection.add({
+    return await groupCollection.doc(id).set({
       "groupName": groupName,
       "groupIcon": "",
-      "admin": "${id}_$userName",
+      "admin":"${id}_$userName",
       "members": [],
-      "groupId": "",
-      "recentMessage": "",
-      "recentMessageSender": "",
+      "groupId":"" ,
+      "recentMessages":"",
+      "recentMessagesSender":"",
     });
-     await groupDocumentReference.update({
-      "members": FieldValue.arrayUnion(["${uid}_$userName"]),
-      "groupId": groupDocumentReference.id,
-    });
+    
   }
 
 }
