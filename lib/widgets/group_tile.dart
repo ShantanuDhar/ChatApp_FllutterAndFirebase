d3a9e301df8568ee025224ad66431d9fa9ee7a41
ShantanuDhar <shantanu170604@gmail.com>
2024-04-11 20:31:15 +0530

    Final design group tile

--- lib/widgets/group_tile.dart
+++ lib/widgets/group_tile.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp_firebase/pages/chat_page.dart';
+import 'package:chatapp_firebase/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class GroupTile extends StatefulWidget {
@@ -19,10 +21,38 @@ class GroupTile extends StatefulWidget {
 class _GroupTileState extends State<GroupTile> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(widget.groupName),
-      subtitle: Text(widget.groupId),
+    return GestureDetector(
+      onTap: (){
+        nextScreen(context, ChatPage());
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
+        child: ListTile(
+          leading: CircleAvatar(backgroundColor: Theme.of(context).primaryColor ,
+          radius: 30,
+          child: Text(
+            widget.groupName.substring(0,1).toUpperCase(),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+          )
+          )
 
+          
+          
+          ),
+          title: Text(widget.groupName,
+          style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text("Join the conversation as ${widget.userName}",
+          style: TextStyle(
+            fontSize: 13
+          ),
+          )
+        ),
+      ),
     );
   }
 }
