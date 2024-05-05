bac4aee6e08ed9e92d3e4b8e3e6b326666dfaee6
ShantanuDhar <shantanu170604@gmail.com>
2024-05-05 18:03:42 +0530

    Designing the search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,8 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,8 +12,76 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+TextEditingController searchController= TextEditingController();
+bool isLoading=false;
+bool hasUserSearched= false;
+String userName=" ";
+QuerySnapshot? searchSnapshot;
+bool isJoined= false;
+User? user;
+
+@override
+  void initState() {
+    super.initState();
+    getUserIdAndName();
+  }
+getUserIdAndName() async {
+  user=FirebaseAuth.instance.currentUser!;
+  await HelperFunction.getUserName().then((val){
+    setState((){
+      userName=val!;
+
+    });
+  });
+}
+
+
+  String getName(String r) {
+    return r.substring(r.indexOf("_") + 1);
+  }
+
+  String getId(String res) {
+    return res.substring(0, res.indexOf("_"));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Search",
+          style: TextStyle(
+              fontSize: 27, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+             color: Theme.of(context).primaryColor,
+               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+               child:Row(
+                children: [
+                  Expanded(child: TextField( 
+                    controller: searchController,
+                     style: const TextStyle(color: Colors.white),
+                       decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search groups....",
+                        hintStyle:
+                            TextStyle(color: Colors.white, fontSize: 16)),
+                  
+                
+
+
+                  ))
+                ],
+               )
+          )
+        ],
+      ),
+
+    );
   }
 }
