89507eb5c1d0f17a8925b4ce539574d76f61206a
ShantanuDhar <shantanu170604@gmail.com>
2024-05-06 23:39:18 +0530

    Almighty push

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,29 +6,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
-  final String userName;
-  final String groupName;
   final String groupId;
-
-  const ChatPage({super.key, required this.groupId, required this.groupName, required this.userName});
+  final String groupName;
+  final String userName;
+  const ChatPage(
+      {Key? key,
+      required this.groupId,
+      required this.groupName,
+      required this.userName})
+      : super(key: key);
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  String admin="";
   Stream<QuerySnapshot>? chats;
   TextEditingController messageController = TextEditingController();
+  String admin = "";
 
   @override
   void initState() {
-    super.initState();
     getChatandAdmin();
-    
-
+    super.initState();
   }
-     getChatandAdmin() {
+
+  getChatandAdmin() {
     DatabaseService().getChats(widget.groupId).then((val) {
       setState(() {
         chats = val;
@@ -40,40 +43,33 @@ class _ChatPageState extends State<ChatPage> {
       });
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
         centerTitle: true,
         elevation: 0,
-        title: Text(widget.groupName, style: const TextStyle(color: Colors.white, fontSize: 27, fontWeight: FontWeight.bold)),
+        title: Text(widget.groupName),
+        backgroundColor: Theme.of(context).primaryColor,
         actions: [
           IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.search , size: 33, ),
-          ),
-          IconButton(
-            onPressed: (){
-              nextScreen(context, GroupInfo(
-                groupId: widget.groupId,
-                groupName: widget.groupName,
-                adminName: admin,
-              ));
-            },
-            icon: const Icon(Icons.more_vert, size: 33,),
-          )
+              onPressed: () {
+                nextScreen(
+                    context,
+                    GroupInfo(
+                      groupId: widget.groupId,
+                      groupName: widget.groupName,
+                      adminName: admin,
+                    ));
+              },
+              icon: const Icon(Icons.info))
         ],
-
-
-
-
       ),
       body: Stack(
-        children: <Widget>
-        [
-          // chatMessages(),
+        children: <Widget>[
+          // chat messages here
+          chatMessages(),
           Container(
             alignment: Alignment.bottomCenter,
             width: MediaQuery.of(context).size.width,
@@ -81,10 +77,9 @@ class _ChatPageState extends State<ChatPage> {
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
               width: MediaQuery.of(context).size.width,
               color: Colors.grey[700],
-              child:Row(
-                children: [
-                  Expanded(
-                     child: TextFormField(
+              child: Row(children: [
+                Expanded(
+                    child: TextFormField(
                   controller: messageController,
                   style: const TextStyle(color: Colors.white),
                   decoration: const InputDecoration(
@@ -114,48 +109,46 @@ class _ChatPageState extends State<ChatPage> {
                     )),
                   ),
                 )
-                ],
-              ),
+              ]),
             ),
-
           )
-
-
         ],
       ),
-      
     );
   }
-  chatMessages(){
-    StreamBuilder(
+
+  chatMessages() {
+    return StreamBuilder(
       stream: chats,
-      builder: (context, AsyncSnapshot snapshot){
-        return snapshot.hasData?
-         ListView.builder(itemCount: snapshot.data.docs.length ,
-          itemBuilder: (context,index){
-           return MessageTile( message: snapshot.data.docs[index]['message'],
+      builder: (context, AsyncSnapshot snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (context, index) {
+                  return MessageTile(
+                      message: snapshot.data.docs[index]['message'],
                       sender: snapshot.data.docs[index]['sender'],
-                      sentByMe: widget.userName== snapshot.data.docs[index]['sender']);
-
-
-        }
-        ):Container();
-      } ,
+                      sentByMe: widget.userName ==
+                          snapshot.data.docs[index]['sender']);
+                },
+              )
+            : Container();
+      },
     );
-
   }
-  sendMessage(){
-    
-if(messageController.text.isNotEmpty){
-  Map<String,dynamic> chatMessageMap= {
-    "message":messageController.text,
-    "sender": widget.userName,
-    "time": DateTime.now().millisecondsSinceEpoch,
-  };
-    DatabaseService().sendMessage(widget.groupId, chatMessageMap);
+
+  sendMessage() {
+    if (messageController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "message": messageController.text,
+        "sender": widget.userName,
+        "time": DateTime.now().millisecondsSinceEpoch,
+      };
+
+      DatabaseService().sendMessage(widget.groupId, chatMessageMap);
       setState(() {
         messageController.clear();
       });
-}
+    }
   }
 }
