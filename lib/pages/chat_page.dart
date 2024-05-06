fea5ad20a551fe2320d2362e9784947e8f474432
ShantanuDhar <shantanu170604@gmail.com>
2024-05-06 22:23:50 +0530

    Adding message_tile

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -18,6 +18,7 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   String admin="";
   Stream<QuerySnapshot>? chats;
+  TextEditingController messageController = TextEditingController();
 
   @override
   void initState() {
@@ -68,6 +69,66 @@ class _ChatPageState extends State<ChatPage> {
 
 
       ),
+      body: Stack(
+        children: <Widget>
+        [
+          // chatMessages(),
+          Container(
+            alignment: Alignment.bottomCenter,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey[700],
+              child:Row(
+                children: [
+                  Expanded(
+                     child: TextFormField(
+                  controller: messageController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    hintText: "Send a message...",
+                    hintStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    border: InputBorder.none,
+                  ),
+                )),
+                const SizedBox(
+                  width: 12,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    sendMessage();
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: const Center(
+                        child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    )),
+                  ),
+                )
+                ],
+              ),
+            ),
+
+          )
+
+
+        ],
+      ),
+      
     );
+  }
+  chatMessages(){
+
+  }
+  sendMessage(){
+
   }
 }
